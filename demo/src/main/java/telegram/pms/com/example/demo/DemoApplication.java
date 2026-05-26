589a8373f0de8a0e7ec7a07d58a61d0e15a9ce03
LAPTOP-I5I4QM2N\jcleo <jinchuen.leong@qrra.com>
2026-05-27 03:19:38 +0800

    added - ConfigurationPropertiesScan
    added - EnableScheduling to run polling every few second

--- demo/src/main/java/telegram/pms/com/example/demo/DemoApplication.java
+++ demo/src/main/java/telegram/pms/com/example/demo/DemoApplication.java
@@ -2,12 +2,17 @@ package telegram.pms.com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling // Allows Spring to run polling every few seconds.
+
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
